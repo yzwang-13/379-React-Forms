143d10fbb4e089f0b9217cf0f0861ea4c202e967
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-13 22:54:29 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [finalName, setFinalName] = useState("");
+
+  function handleChange(event) {
+    // console.log("changed");
+    console.log(event.target.value);
+    setName(event.target.value);
+    // console.log(event.target.placeholder);
+    // console.log(event.target.type);
+  }
+
+  function greet(event) {
+    setFinalName(name);
+  }
+
+  function handleFormSubmit(event) {
+    // prevent from refreshing the page
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {finalName}</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit" onClick={greet}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
